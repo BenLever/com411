952d0e0330f51264acdad35981dafe3781c0f6d6
repl.it user <replituser@example.com>
2020-09-29 10:22:36 +0000

    Updated code to display a messgae with escape characters

--- basics/output/escape_characters.py
+++ basics/output/escape_characters.py
@@ -2,5 +2,7 @@
 print("\n Displays a new line")
 print("\t Displays a tab space")
 print("\\ Displays  a back slash")
-print("\ Displays a double quote")
+print("\" Displays a double quote")
 print("\' Displays a single quote")
+print("\n")
+print("\t \"I am Beep!\"")
